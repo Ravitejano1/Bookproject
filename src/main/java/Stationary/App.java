21d4c35235da0c43cb271a3823a806d5be863bcc
Ravitejano1 <94282413+Ravitejano1@users.noreply.github.com>
2022-10-26 15:41:18 +0530

    Add files via upload

---
+++ src/main/java/Stationary/App.java
@@ -0,0 +1,13 @@
+package Stationary;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
